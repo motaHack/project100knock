9ecfbdeae8628903eaf150090acdb971a84ad652
ssszip <firstacom_y9emo@icloud.com>
2017-03-22 23:03:10 +0900

    4回目のループからなぜか無限ループになるけど今日は眠いからここまで

--- 100knock/007.rb
+++ 100knock/007.rb
@@ -1,1 +1,28 @@
-# てすと
+i = 13
+
+flg = true
+primes = []
+
+while primes.size <= 1 do
+  n = 2
+  while n < i do
+    if i % n == 0
+      flg = false
+      break
+    end
+    n += 1
+  end
+
+  if flg == true
+    primes.push(i)
+  end
+
+  if i == 2
+    i += 1
+  else
+    i += 2
+  end
+  print(i,"\n")
+end
+
+print(primes,"\n")
