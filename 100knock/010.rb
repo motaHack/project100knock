31f7a52304fd983c64847c5264f4ea9c4195d709
ssszip <firstacom_y9emo@icloud.com>
2017-03-25 20:59:33 +0900

    No.10

---
+++ 100knock/010.rb
@@ -0,0 +1,26 @@
+include Math
+
+start_time = Time.now
+i = 2
+sum = 0
+n = 2
+
+def isPrime(i)
+  n = 2
+  for n in 2..sqrt(i)  do
+    if i % n == 0
+      return false
+    end
+    n += 1
+  end
+end
+
+for i in 2..2000000 do
+  if isPrime(i)
+    sum += i
+  end
+  i += 1
+end
+
+puts(sum)
+puts(Time.now - start_time)
