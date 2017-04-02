ec09b0d4f74a317c413f35c5d3d2dd6a3239c14f
ssszip <firstacom_y9emo@icloud.com>
2017-04-02 21:10:50 +0900

    もっとおしゃれに解きたい

--- 100knock/015.rb
+++ 100knock/015.rb
@@ -1,6 +1,17 @@
 start_time = Time.now
 
 # 40C20
+# ans = 40!/20!/20!
+# ans = 40*39*...*21/20*19*...*1
 
+ans = 1
+40.downto(21){|n|
+ ans *= n
+}
 
+20.downto(1){|m|
+  ans /= m
+}
+
+puts(ans)
 puts(Time.now - start_time)
