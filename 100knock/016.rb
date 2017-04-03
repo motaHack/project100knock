aefd25091e22aab3115c059dbbb2b205993bbef8
ssszip <firstacom_y9emo@icloud.com>
2017-04-03 23:58:58 +0900

    残りは明日

--- 100knock/016.rb
+++ 100knock/016.rb
@@ -1,10 +1,15 @@
 start_time = Time.now
 i = 0
 pro = 1
+sum = 0
 
 for i in 1..1000 do
   pro *= 2
 end
 
-puts(pro)
+(pro.to_s).chars{|s|
+  sum += (s.to_i)
+}
+
+puts(sum)
 puts(Time.now - start_time)
