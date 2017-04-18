5860c6b3c62a4039fb3a219100d93fe929a703e5
ssszip <firstacom_y9emo@icloud.com>
2017-04-18 23:32:19 +0900

    洗濯物干すから一旦コミット

--- 100knock/017.rb
+++ 100knock/017.rb
@@ -1,4 +1,4 @@
- start_time = Time.now
+start_time = Time.now
 
 num = {1 => "one",2 => "two",3 => "three",4 => "four",5 => "five",6 => "six",7 => "seven",8 => "eight",9 => "nine",10 => "ten",11 => "eleven",12 => "twelve",13 => "thirteen",14 => "fourteen",15 => "fifteen",16 => "sixteen",17 => "seventeen",18 => "eighteen",19 => "nineteen",20 => "twenty",30 => "thirty",40 => "forty",50 => "fifty",60 => "sixty",70 => "seventy",80 => "eighty",90 => "ninety",100 => "hundred",1000 => "onethousand"}
 
@@ -6,9 +6,10 @@ i = 1
 total = 0
 
 for i in 1..1000 do
-  case i
 
+  case i
   when 1..20
+    puts(num[i])
     total += String(num[i]).length
 
   when 21..99
@@ -18,10 +19,14 @@ for i in 1..1000 do
     total += String(num[(i-n)]).length + String(num[n]).length
 
   when 100..999
-    n = i % 100 //しもふたけた
-    m = n % 10 //しもいちけた
-    l = (i - n)/100 //かみひとけた
-    total += String(num[l]).length + String(num[100]).length String(num[n-m]).length + String(num[m]).length
+    n = i % 100 #しもふたけた
+    m = n % 10 #しもひとけた
+    l = (i - n)/100 #かみひとけた
+    puts(num[l])
+    puts(num[100])
+    puts(num[n-m])
+    puts(num[m])
+    total += String(num[l]).length + String(num[100]).length + String(num[n-m]).length + String(num[m]).length
 
   else
     total += String(num[1000]).length
