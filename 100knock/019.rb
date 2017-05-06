606329a174f84edfb2a727dd9fe725f2deae0fdf
ssszip <firstacom_y9emo@icloud.com>
2017-05-06 23:26:01 +0900

    試行錯誤中

--- 100knock/019.rb
+++ 100knock/019.rb
@@ -1,12 +1,20 @@
 start_time = Time.now
 sunday = 0
+domi = 1900
+month = 1
+date = 1
 
-while i < 2001 do
+while domi < 2001 do
+  # うるう年
+  if (domi % 4 == 0)
+  && !((domi % 400 != 0) && (domi % 100 == 0))
+    
 
+  else
+    # うるう年ではない
+
+  end
 end
 
-# うるう年ではない
-
-# うるう年
-
+p sunday
 puts(Time.now - start_time)
