bf1469d98b35a8a59b2daf2ad4a11a68d42bf270
ssszip <firstacom_y9emo@icloud.com>
2017-05-09 22:50:16 +0900

    case文追加

--- 100knock/019.rb
+++ 100knock/019.rb
@@ -16,7 +16,33 @@ end
 while year < 2001 do
   while month <= 12 do
 
-    # switch
+    case month
+    when 1
+
+    when 2
+
+    when 3
+
+    when 4
+
+    when 5
+
+    when 6
+
+    when 7
+
+    when 8
+
+    when 9
+
+    when 10
+
+    when 11
+
+    when 12
+
+    end
+  
     month += 1
   end
   month = 1
