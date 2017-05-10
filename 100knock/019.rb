4b2996c9e8264525835ed707be8855a87d991f57
ssszip <firstacom_y9emo@icloud.com>
2017-05-10 22:37:48 +0900

    途中

--- 100knock/019.rb
+++ 100knock/019.rb
@@ -2,6 +2,7 @@ start_time = Time.now
 sunday = 0
 year = 1900
 month = 1
+day = 0
 
 def uruu(year)
   if year % 4 != 0
@@ -13,16 +14,23 @@ def uruu(year)
   end
 end
 
-while year < 2001 do
+while year <= 2000 do
   while month <= 12 do
 
     case month
     when 1
-
+      sunday += 1 if day % 7 == 0
+      day += 31
     when 2
-
+      sunday += 1 if day % 7 == 0
+      if !uruu(year)
+        day += 28
+      else
+        day += 29
+      end
     when 3
-
+      sunday += 1 if day % 7 == 0
+      day += 31
     when 4
 
     when 5
@@ -42,7 +50,6 @@ while year < 2001 do
     when 12
 
     end
-  
     month += 1
   end
   month = 1
