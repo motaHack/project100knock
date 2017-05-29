9cded6b7b64004f14a2740640eb9e33e1cd160f1
ssszip <firstacom_y9emo@icloud.com>
2017-05-29 22:47:02 +0900

    配列に入れるところまで

--- 100knock/022.rb
+++ 100knock/022.rb
@@ -1,9 +1,17 @@
 start_time = Time.now
+str = nil
+names = []
 
 texts = File.open("names.txt","r") do |file|
   file.each_line do |line|
-    puts line
+     str = line
   end
 end
+
+str = str.delete("\"")
+names = str.split(",")
+p names
+
+# そーとする
 
 puts(Time.now - start_time)
