f1b0b6c58b1695f9a5b68090cf4fe63203769494
ssszip <firstacom_y9emo@icloud.com>
2017-05-31 23:17:41 +0900

    1文字の点数を出すところまで

--- 100knock/022.rb
+++ 100knock/022.rb
@@ -1,6 +1,7 @@
 start_time = Time.now
 str = nil
 names = []
+score = 0
 
 texts = File.open("names.txt","r") do |file|
   file.each_line do |line|
@@ -10,5 +11,14 @@ end
 str = str.delete("\"").delete("\n")
 names = str.split(",")
 names = names.sort
+p names
 
+
+names.each do |name|
+  name.each_char do |chr|
+    score += chr.ord - 64
+  end
+end
+
+p score
 puts(Time.now - start_time)
