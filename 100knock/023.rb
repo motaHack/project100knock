97dfdf7e30bdcc67cf3e4d728bd70df74e8c5eef
ssszip <firstacom_y9emo@icloud.com>
2017-06-04 23:03:37 +0900

    途中まで

--- 100knock/023.rb
+++ 100knock/023.rb
@@ -1,4 +1,5 @@
 start_time = Time.now
+i = 0
 
 def sum_divisor(num)
   divs = (1..(num/2)).select {|i|
@@ -9,6 +10,13 @@ def sum_divisor(num)
     sum += i
   }
   return sum
+end
+
+while i*2 < 28123 do
+  if i < sum_divisor(i)
+    p i * 2
+  end
+  i += 1
 end
 
 puts(Time.now - start_time)
