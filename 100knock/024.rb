a732690383420a0d4f61aa25b38bc106b3c5df8a
ssszip <firstacom_y9emo@icloud.com>
2017-06-10 23:25:35 +0900

    再帰を勉強する必要がある

--- 100knock/024.rb
+++ 100knock/024.rb
@@ -1,23 +1,9 @@
 start_time = Time.now
-total = 0
-num = [2,4,6,8,10,12,14,16,18,20]
+nums = [0,1,2,3,4,5,6,7,8,9]
+first = 0
 
-for i in 1..20 do
-  if i%2 == 0 then
-    total += i
-  end
+for i in 1..1000000 do
+
 end
-p total
-
-total = 0
-for j in 1..20 do
-  total += j
-end
-
-num.each{ |a|
-  total = total - a
-}
-
-p total
 
 puts(Time.now - start_time)
