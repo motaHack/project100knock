91b7b4d62aac8e062c7d1498a03bb7951591230e
ssszip <firstacom_y9emo@icloud.com>
2017-06-16 23:56:29 +0900

    途中

---
+++ 100knock/4.rb
@@ -0,0 +1,6 @@
+start_time = Time.now
+str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+arr = str.split(/[\s,\.]{1,2}/)
+answer = [:]
+
+puts(Time.now - start_time)
