65a323ef3506f0aa0a47e1becd5c381acfe0d2ce
ssszip <firstacom_y9emo@icloud.com>
2017-06-16 23:44:43 +0900

    修正

--- 100knock/3.rb
+++ 100knock/3.rb
@@ -1,6 +1,6 @@
 start_time = Time.now
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
-p (str.split(/[\s,\.]/)).select {|s| s != ""}
+p str.split(/[\s,\.]{1,2}/)
 
 puts(Time.now - start_time)
