b7704435c8ea12792ac47261372e76778f8ca321
ssszip <firstacom_y9emo@icloud.com>
2017-06-17 23:06:58 +0900

    回答完成

--- 100knock/3.rb
+++ 100knock/3.rb
@@ -1,6 +1,9 @@
 start_time = Time.now
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+arr = str.split(/[\s,\.]{1,2}/)
+arr = arr.map! { |word|
+  word.length
+}
 
-p str.split(/[\s,\.]{1,2}/)
-
+p arr
 puts(Time.now - start_time)
