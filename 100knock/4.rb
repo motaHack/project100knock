e12dc8a6cc35638df011afafb980eb85c473dd29
ssszip <firstacom_y9emo@icloud.com>
2017-06-18 10:19:23 +0900

    完成

--- 100knock/4.rb
+++ 100knock/4.rb
@@ -1,6 +1,16 @@
 start_time = Time.now
 str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 arr = str.split(/[\s,\.]{1,2}/)
-answer = [:]
+count = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+answer = Hash.new
 
+arr.each do |word|
+  if count.include?(arr.index(word) + 1)
+    answer[arr.index(word)] = word[0]
+  else
+    answer[arr.index(word)] = (word[0] + word[1])
+  end
+end
+
+p answer
 puts(Time.now - start_time)
