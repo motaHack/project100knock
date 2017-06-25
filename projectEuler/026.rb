49f116526d7d1ee87ebf6bf6978b07f59b6efff2
ssszip <firstacom_y9emo@icloud.com>
2017-06-25 23:20:10 +0900

    激遅なので要修正

--- projectEuler/026.rb
+++ projectEuler/026.rb
@@ -1,9 +1,33 @@
 start_time = Time.now
+answer = 0
+max_length = 0
 
-for d in 1..999 do
+def junkan_length(num)
+  m = 1
+  s = 0
+  t = 0
+  list = []
+
+  until list.include?(m) do
+    list[s] = m
+    m = (m * 10) % num
+    if m == 0
+      break
+    end
+    t += 1
+  end
+  return t + 1
+end
+
+for d in 1..5 do
   if d % 2 != 0
-    n = (1.to_f)/(d.to_f)
+    l = junkan_length(d)
+    if l > max_length
+      answer = d
+      max_length = l
+    end
   end
 end
 
+p answer
 puts(Time.now - start_time)
