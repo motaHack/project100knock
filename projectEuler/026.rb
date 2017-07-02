4553c4887a8acc334a7c64c0d07b35d7acc6a0e3
ssszip <firstacom_y9emo@icloud.com>
2017-07-02 22:55:11 +0900

    一旦保留

--- projectEuler/026.rb
+++ projectEuler/026.rb
@@ -3,44 +3,12 @@ answer = 0
 max_length = 0
 
 def junkan_length(num)
-  m = 1
-  o = 1
-  s = 0
-  t = 0
 
-  loop do
-    m = (m * 10) % num
-    o = (o * 10) % num
-    o = (o * 10) % num
-    if m == o
-      break
-    end
-  end
-
-  if o != 0
-    o = 1
-    s = 1
-
-    while m != o do
-      s += 1
-      m = (m * 10) % num
-      o = (o * 10) % num
-    end
-
-    o = (o * 10) % num
-    t = s
-
-    while m != o do
-      t += 1
-      o = (o * 10) % num
-    end
-  end
-
-  return s
+  return length
 end
 
-for d in 1..7 do
-  print(d,":",junkan_length(d),"\n")
+for d in 1..999 do
+  junkan_length(d)
 end
 p answer
 puts(Time.now - start_time)
