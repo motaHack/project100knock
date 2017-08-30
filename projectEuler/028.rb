b82d8886cc67b591f9a44712967876baff31d150
motaHack <motaHack@users.noreply.github.com>
2017-08-30 23:39:00 +0900

    Update 028.rb

--- projectEuler/028.rb
+++ projectEuler/028.rb
@@ -2,11 +2,15 @@ start_time = Time.now
 
 #  (2n-1)^2 + (2n-1)^2 + {(2n-1)^2-(2n-2)} + {(2n-1)^2-(2n-2)*2} + {(2n-1)^2-(2n-2)*3}...
 
-i = 2
-sum = 1
+i = 1
+sum = 0
 while i <= 501 do
-  sum += 4*(2 * i - 1)**2 - 6*(2 * i -2)
-  i += 1
+  if i = 1
+    sum = 1
+  else
+   sum += 4*(2 * i - 1)**2 - 6*(2 * i -2)
+   i += 1
+  end
 end
 p sum
 puts(Time.now - start_time)
