8546deab47c3113ef431a215a9946ee4bdc25484
ssszip <firstacom_y9emo@icloud.com>
2017-09-04 22:36:49 +0900

    完成した（が、もっと早く出来そう

--- projectEuler/029.rb
+++ projectEuler/029.rb
@@ -3,14 +3,19 @@ arr = []
 a = 2
 b = 2
 
-while (a <= 100) && (b <= 100) do
+while (a <= 100) do
   c = a ** b
+  p c
   if !arr.include?(c)
     arr << c
   end
-  a += 1
+
+  if b == 100
+    b = 1
+    a += 1
+  end
   b += 1
 end
 
-p arr
+p arr.size()
 puts(Time.now - start_time)
