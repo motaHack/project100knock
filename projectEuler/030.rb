703072ae7a8ce2e5a76b2e2a1eaea46be5011c96
ssszip <firstacom_y9emo@icloud.com>
2017-09-10 23:56:26 +0900

    muzui

--- projectEuler/030.rb
+++ projectEuler/030.rb
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 start_time = Time.now
 a = 2
 sum = 0
 
-(a.to_s).chars { |ch|
-  n = ch.to_i
-  sum += n ** 5
-}
+for i in 1..20 do
+  (a.to_s).chars { |ch|
+    n = ch.to_i
+    sum += n ** 5
+  }
+end
 
-p sum
+
 puts(Time.now - start_time)
-=======
-
->>>>>>> 881efcb601c64cdaa15c311571687ebcfac4ee28
