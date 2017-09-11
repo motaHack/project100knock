2bc3349b8c56b2b7595db73255dc503951edc59f
ssszip <firstacom_y9emo@icloud.com>
2017-09-11 23:25:21 +0900

    途中

--- projectEuler/030.rb
+++ projectEuler/030.rb
@@ -1,13 +1,17 @@
 start_time = Time.now
 a = 2
 sum = 0
+answer = 0
+limit = (9**5) * 6
 
-for i in 1..20 do
+while sum < limit do
   (a.to_s).chars { |ch|
     n = ch.to_i
     sum += n ** 5
   }
+  answer = 2 if sum == a
+  a += 1
 end
 
-
+p answer
 puts(Time.now - start_time)
