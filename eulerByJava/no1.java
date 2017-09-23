f5655853e800be0ea14a924e79f619ac34c4ed1d
ssszip <firstacom_y9emo@icloud.com>
2017-09-23 23:45:56 +0900

    complete

--- eulerByJava/no1.java
+++ eulerByJava/no1.java
@@ -1,4 +1,13 @@
 public class no1 {
-  public static void main(Strings args[]) {
+  public static void main(String[] args) {
+    int i = 1;
+    int sum = 0;
+    while (i < 1000) {
+      if (i%3 == 0 || i%5 == 0) {
+        sum += i;
+      }
+      i ++;
+    }
+    System.out.println(sum);
   }
 }
