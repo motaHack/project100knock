ae3181616df0786a1bd27395fc01d347647d8f7e
ssszip <firstacom_y9emo@icloud.com>
2017-09-25 22:34:06 +0900

    complete

--- eulerByJava/no2.java
+++ eulerByJava/no2.java
@@ -1,4 +1,21 @@
-public class no2{
+public class no2 {
     public static void main(String[] args){
+      int a = 1;
+      int b = 2;
+      int c = 0;
+      long sum = 0;
+
+      while(a < 4000000) {
+        if (a % 2 == 0) {
+          sum += a;
+        }
+        if (b % 2 == 0) {
+          sum += b;
+        }
+        c = b;
+        a += b;
+        b = c + a;
+      }
+      System.out.println(sum);
     }
 }
