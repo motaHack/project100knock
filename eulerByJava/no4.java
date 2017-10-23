db1e52a86ed84ab33cdfec509382b74907d1d5d7
motaHack <motaHack@users.noreply.github.com>
2017-10-23 23:48:54 +0900

    Update no4.java

--- eulerByJava/no4.java
+++ eulerByJava/no4.java
@@ -5,10 +5,12 @@ public class no4 {
   int b = 100;
   String productNum = "";
   
-  // 逆から半分と前から半分が同じならおk
   while (b < 1000) {
     while (a < 1000) {
       productNum = (a * b).toString();
+      if (productNum == productNum.reverse()) {
+        
+      }
       a++;
     }
     b++;
