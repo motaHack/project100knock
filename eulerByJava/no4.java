b7b4fe73dcbb89a4c484b67bbf75071a5adb9839
motaHack <motaHack@users.noreply.github.com>
2017-10-24 23:18:12 +0900

    Update no4.java

--- eulerByJava/no4.java
+++ eulerByJava/no4.java
@@ -3,16 +3,18 @@ import java.lang.*;
 public class no4 {
   int a = 100;
   int b = 100;
+  String max = "";
   String productNum = "";
   
   while (b < 1000) {
     while (a < 1000) {
       productNum = (a * b).toString();
-      if (productNum == productNum.reverse()) {
-        
+      if (productNum.equals(productNum.reverse())) {
+        max = productNum;
       }
       a++;
     }
     b++;
   }
+  System.out.println(max);
 }
