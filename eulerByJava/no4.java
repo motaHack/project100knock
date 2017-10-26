6c8226f94d71a684eecaaede500c1444b3446aaa
ssszip <firstacom_y9emo@icloud.com>
2017-10-26 23:59:24 +0900

    あああ

--- eulerByJava/no4.java
+++ eulerByJava/no4.java
@@ -1,18 +1,19 @@
 import java.lang.*;
 
 public class no4 {
-  public static void main() {
+  public static void main(String[] args) {
     int a = 100;
     int b = 100;
     String max = "";
     String productNum = "";
+
 
     while (b < 1000) {
       while (a < 1000) {
         productNum = String.valueOf(a*b);
         StringBuffer sb = new StringBuffer(productNum);
         String rvs = sb.reverse().toString();
-        if (productNum.equals(productNum.equals(rvs))) {
+        if (productNum.equals(rvs)) {
           max = productNum;
         }
         a++;
